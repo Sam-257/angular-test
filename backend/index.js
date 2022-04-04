2a5854aa646aa64fef4668d4845c355615f12428
Sam-257 <59176780+Sam-257@users.noreply.github,com>
2022-04-04 19:15:46 +0530

    checking for valid event inputs at server-side

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-const cors = require('cors');
-const mysql = require('mysql2');
+const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql2");
 
 const app = express();
 app.use(cors());
@@ -9,69 +9,71 @@ app.use(bodyparser.json());
 
 //Mysql connect
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'angular-test',
-    port: 3306
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "angular-test",
+    port: 3306,
 });
 
 //checking if db is connected
-db.connect(err =>{
-    if(err){
-        console.log(err,'Error while connecting to Database');
-    }
-    else{
-        console.log('Connected to database');
+db.connect((err) => {
+    if (err) {
+        console.error(err, "Error while connecting to Database");
+    } else {
+        console.log("Connected to database");
     }
 });
 
-// Select query API 
-app.get('/event',(req,res)=>{
+// Select query API
+app.get("/event", (req, res) => {
     //console.log('getting event information');
     let qr = "SELECT * FROM events";
-    db.query(qr,(err,result)=>{
-        if(err){console.log(err,'error in select query')}
-        if(result.length > 0){
+    db.query(qr, (err, result) => {
+        if (err) {
+            console.error(err, "error in select query");
+        }
+        if (result.length > 0) {
             res.send({
-                data: result
+                data: result,
             });
         }
     });
 });
 
 // Insert query API
-app.post('/event',(req,res)=>{
+app.post("/event", (req, res) => {
     let title = req.body.title;
     let description = req.body.description;
     let active = 1;
-    //let values = [title,description,active];
-    //console.log(values);
-    let qr = `INSERT INTO events(title, description, active) VALUES ('${title}','${description}','${active}')`;
-    db.query(qr,(err,result)=>{
-        if(err) throw err;
-        console.log(result);
+    if (title == undefined || description == undefined) {
         res.send({
-            message:'data Inserted'
+            message: "Invalid Data",
+        });
+    } else {
+        let qr = `INSERT INTO events(title, description, active) VALUES ('${title}','${description}','${active}')`;
+        db.query(qr, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.send({
+                message: "data Inserted",
+            });
+        });
+    }
+});
+
+// Delete query
+app.delete("/event/:sno", (req, res) => {
+    let sno = req.params.sno;
+    let qr = `DELETE FROM events WHERE sno = '${sno}'`;
+    db.query(qr, (err, result) => {
+        if (err) throw err;
+        res.send({
+            message: "data deleted",
         });
     });
 });
 
-// Delete query
-app.delete('/event/:sno',(req,res)=>{
-    let sno = req.params.sno;
-    let qr = `DELETE FROM events WHERE sno = '${sno}'`;
-    db.query(qr,(err,result)=>{
-        if(err) throw err;
-        res.send({
-            message:'data deleted'
-        })
-    })
-})
-
-
-
-
-app.listen(3002,()=>{
-    console.log('Server running...');
-}); 
+app.listen(3002, () => {
+    console.log("Server running...");
+});
